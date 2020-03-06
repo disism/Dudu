2f979cce1379850f1b41e0d953363c0645fdcab9
disism <x@disism.com>
2020-03-06 13:25:17 +0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import PublicDta from "./componments/public-data";
+
+function App() {
+  return (
+    <>
+        <PublicDta/>
+    </>
+  );
+}
+
+export default App;
