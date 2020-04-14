8ccd9f14fd5f132e7db5fa08fa34fc874ea8100c
disism <x@disism.com>
2020-04-14 16:05:49 +0800

    ADDD STREAMING

--- src/componments/header/index.js
+++ src/componments/header/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./style.scss"
 import {Link} from "react-router-dom";
+import Streaming from "../streaming";
 
 function HeaderComponent() {
     const handleLogout = () => {
@@ -9,16 +10,19 @@ function HeaderComponent() {
     }
 
     return (
-        <header className="header">
-            <Link to="/home">首页</Link>
-            <Link to="/favourites">喜欢</Link>
-            <Link to="/bookmarks">收藏</Link>
-            <Link to="/conversations">私信</Link>
-            <Link to="/notifications">通知</Link>
-            <Link to="/search">搜索</Link>
-            <Link to="/public">公开时间轴</Link>
-            <button type="button" onClick={handleLogout}>退出</button>
-        </header>
+        <>
+            <header className="header">
+                <Link to="/home">首页</Link>
+                <Link to="/favourites">喜欢</Link>
+                <Link to="/bookmarks">收藏</Link>
+                <Link to="/conversations">私信</Link>
+                <Link to="/notifications">通知</Link>
+                <Link to="/search">搜索</Link>
+                <Link to="/public">公开时间轴</Link>
+                <button type="button" onClick={handleLogout}>退出</button>
+            </header>
+            <Streaming />
+        </>
     )
 }
 
--- src/componments/streaming/index.js
+++ src/componments/streaming/index.js
@@ -1,31 +1,38 @@
-import React from "react";
-import {getStreamingHealth, getStreamingUser} from "../../api/request";
+import React, {useEffect} from "react";
 
 function Streaming() {
-    const handleCheckStreamingHealth = () => {
-        getStreamingHealth()
-            .then(res => {
-                console.log(res)
-            })
-            .catch(err => {
-                console.log(err)
-            })
-    }
 
-    const handleGetStreamingUser = () => {
-        getStreamingUser()
-            .then(res => {
-                console.log(res)
-            })
-            .catch(err => {
-                console.log(err)
-            })
-    }
+    const access_token = localStorage.getItem('dudu_access_token')
+    const streamApiUrl = localStorage.getItem('dudu_settings_url').replace('https://', 'wss://')
+
+    useEffect(() => {
+        const ws = new WebSocket(`${streamApiUrl}/api/v1/streaming/?stream=user&access_token=${access_token}&user`)
+        // 链接成功
+        ws.onopen = () => {
+            console.log('连接服务端WebSocket成功');
+        };
+
+        // 监听服务端消息(接收消息)
+        ws.onmessage = e => {
+            console.log(e.data)
+        };
+
+        // 监听连接失败
+        ws.onerror = () => {
+            console.log('连接失败，正在重连...');
+
+        };
+
+        // 监听连接关闭
+        ws.onclose = () => {
+            console.log('连接关闭');
+        };
+
+    },[streamApiUrl, access_token])
 
     return (
         <>
-            <button className="button" onClick={handleCheckStreamingHealth}>查看流状态</button>
-            <button className="button" onClick={handleGetStreamingUser}>查看用户流</button>
+
         </>
     )
 }
